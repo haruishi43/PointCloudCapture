d6adf9f4a5b5f5ce1da35c0ba7674c8fb749a177
Toraudonn <toraudonn@gmail.com>
2018-10-10 12:27:10 +0900

    Initial Commit

---
+++ PointCloudCapture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PointCloudCapture
+//
+//  Created by Haruya Ishikawa on 2018/10/10.
+//  Copyright © 2018 Haruya Ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
